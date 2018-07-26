4bb075b0c6927305aefc5c8eaee3bef16122c2b3
serendipidoussophia <scurranmoore@gmail.com>
2018-07-26 16:36:33 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -18,7 +18,7 @@ def recipe_ingredients(perfect_10_recipe)
 end
 
 def amount_of_chocolate_chips(perfect_10_recipe)
-  perfect_10_recipe{mini_dark_chocolate_chips}
+  perfect_10_recipe{:mini_dark_chocolate_chips}
 end
 
 def ingredients_and_amounts(perfect_10_recipe)
